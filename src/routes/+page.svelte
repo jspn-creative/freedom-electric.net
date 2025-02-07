cba869079067fb344e497c99f1f2478b457fd196
Josh Spinney <josh@jaspin.io>
2025-02-06 18:09:16 -0800

    Adjust scaling, add overflow shadow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 </script>
 
 <div class="relative h-screen w-screen flex items-center">
-  <div class="mx-auto flex max-md:flex-col max-h-screen h-fit">
+  <div class=" flex max-md:flex-col max-h-screen h-fit w-full">
     <!-- Left Side -->
     <div class="flex flex-col justify-between items-center md:items-end pt-12 pb-8 md:p-20 gap-4">
       <!-- Upper -->
@@ -39,93 +39,100 @@
     </div>
 
     <!-- Right Side -->
-    <div class="md:grow md:max-h-screen md:my-10 text-stone-500">
+    <div class="md:grow md:max-h-screen text-stone-500">
       <!-- bg -->
       <div class="md:absolute inset-y-0 w-full bg-stone-50 -z-10"></div>
-      <!-- content -->
-      <div class="mx-4 sm:mx-6 md:mx-0 md:w-full max-md:py-12 py-20 px-10 lg:px-20 space-y-14 h-full overflow-y-scroll max-md:rounded-md max-md:mb-10 max-md:bg-stone-50">
-        <section class="">
-          <h2 class="font-serif mb-4 text-3xl font-bold text-stone-600">About Us.</h2>
-          <p class="md:max-w-xl lg:max-w-lg leading-relaxed">Freedom Electric specializes in industrial electrical contracting, providing professional electrical services with a commitment to excellence and safety. Our experienced team serves businesses like yours with dedication and expertise.</p>
-        </section>
 
-        <section class="">
-          <div class="border-t border-stone-200 pt-10">
-            <div class="rounded-md">
-              <h2 class="font-serif text-3xl font-bold text-stone-600 mb-8">Our Locations:</h2>
+      <div class="relative md:h-screen md:py-10">
+        <!-- content -->
+        <div class="static overflow-y-scroll max-h-full mx-4 sm:mx-6 md:mx-0 md:w-full max-md:py-12 py-20 lg:py-10 px-10 space-y-14 max-md:rounded-md max-md:mb-10 max-md:bg-stone-50">
+          <section class="2xl:mr-[10vw] 2xl:ml-[5vw]">
+            <h2 class="font-serif mb-4 text-3xl font-bold text-stone-600">About Us.</h2>
+            <p class="max-w-2xl leading-relaxed">Freedom Electric specializes in industrial electrical contracting, providing professional electrical services with a commitment to excellence and safety. Our experienced team serves businesses like yours with dedication and expertise.</p>
+          </section>
 
-              <div class="flex md:max-lg:flex-col gap-6 max-w-xl lg:max-w-lg">
-                <div class="rounded-md outline-2 outline-stone-200 py-4 pl-6 pr-8">
-                  <h4 class="font-serif font-semibold text-stone-500 text-sm/4">Oregon Office</h4>
-                  <p class="text-stone-400 text-xs">Tillamook, Oregon</p>
-                </div>
-                <div class="rounded-md outline-2 outline-stone-200 py-4 pl-6 pr-8">
-                  <h4 class="font-serif font-semibold text-stone-500 text-sm/4">Texas Office</h4>
-                  <p class="text-stone-400 text-xs">Winona, Texas</p>
+          <section class="2xl:mr-[10vw] 2xl:ml-[5vw]">
+            <div class="border-t border-stone-200 pt-10">
+              <div class="rounded-md">
+                <h2 class="font-serif text-3xl font-bold text-stone-600 mb-8">Our Locations:</h2>
+
+                <div class="flex md:max-lg:flex-col gap-6">
+                  <div class="rounded-md outline-2 outline-stone-200 py-4 pl-6 pr-8">
+                    <h4 class="font-serif font-semibold text-stone-500 text-sm/4">Oregon Office</h4>
+                    <p class="text-stone-400 text-xs">Tillamook, Oregon</p>
+                  </div>
+                  <div class="rounded-md outline-2 outline-stone-200 py-4 pl-6 pr-8">
+                    <h4 class="font-serif font-semibold text-stone-500 text-sm/4">Texas Office</h4>
+                    <p class="text-stone-400 text-xs">Winona, Texas</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </section>
+          </section>
 
-        <section class="">
-          <div class="border-t border-stone-200 pt-10">
-            <h2 class="font-serif text-3xl font-bold mb-4 text-stone-600">Contact Us:</h2>
-            <p class="mb-6 leading-relaxed">Fill out the form below to get in touch.</p>
+          <section class="2xl:mr-[10vw] 2xl:ml-[5vw]">
+            <div class="border-t border-stone-200 pt-10">
+              <h2 class="font-serif text-3xl font-bold mb-4 text-stone-600">Contact Us:</h2>
+              <p class="mb-6 leading-relaxed">Fill out the form below to get in touch.</p>
 
-            <!--             
+              <!--             
             <div class="mt-6 flex max-w-md gap-x-4">
               <label for="email-address" class="sr-only">Email address</label>
               <Input type="email" required placeholder="Enter your email" class="" />
               <Button type="submit" variant="outline">Subscribe</Button>
             </div> -->
 
-            {#if form?.success}
-              <div id="message" class="text-emerald-500 font-medium text-base">Got it! We'll be in touch.</div>
-            {:else}
-              <form method="POST" action="#message" class="">
-                {#if form?.sendFailed}
-                  <div id="message" class="text-red-400 font-medium text-base/5 pb-2">There was a problem sending your message. Please try again or give us a call to let us know something went wrong.</div>
-                {:else if form?.validationFailed}
-                  <div id="message" class="text-red-400 font-medium text-base pb-2">There seems to be a problem. Please refresh the page and try again.</div>
-                {/if}
-                <div class="max-w-full mx-auto sm:max-w-xl lg:max-w-lg **:has-[+:required]:after:content-['*'] **:has-[+:required]:after:text-red-900/40 **:has-[+:required]:after:text-sm **:has-[+:required]:after:font-medium **:has-[+:required]:after:pl-0.5">
-                  <div class="grid grid-cols-1 gap-x-8 gap-y-2 sm:grid-cols-2">
-                    <div>
-                      <label for="first-name" class="block text-sm/8 font-medium">First name</label>
+              {#if form?.success}
+                <div id="message" class="text-emerald-500 font-medium text-base">Got it! We'll be in touch.</div>
+              {:else}
+                <form method="POST" action="#message" class="">
+                  {#if form?.sendFailed}
+                    <div id="message" class="text-red-400 font-medium text-base/5 pb-2">There was a problem sending your message. Please try again or give us a call to let us know something went wrong.</div>
+                  {:else if form?.validationFailed}
+                    <div id="message" class="text-red-400 font-medium text-base pb-2">There seems to be a problem. Please refresh the page and try again.</div>
+                  {/if}
+                  <div class="max-w-full mx-auto md:ml-0 lg:max-w-2xl **:has-[+:required]:after:content-['*'] **:has-[+:required]:after:text-red-900/40 **:has-[+:required]:after:text-sm **:has-[+:required]:after:font-medium **:has-[+:required]:after:pl-0.5">
+                    <div class="grid grid-cols-1 gap-x-8 gap-y-2 sm:grid-cols-2">
+                      <div>
+                        <label for="first-name" class="block text-sm/8 font-medium">First name</label>
 
-                      <Input type="text" name="first-name" required placeholder="Your first name" class="w-full" />
+                        <Input type="text" name="first-name" required placeholder="Your first name" class="w-full" />
+                      </div>
+                      <div>
+                        <label for="last-name" class="block text-sm/8 font-medium">Last name</label>
+
+                        <Input type="text" name="last-name" required placeholder="Your last name" class="w-full" />
+                      </div>
+                      <div class="sm:col-span-2">
+                        <label for="email" class="block text-sm/8 font-medium">Email</label>
+
+                        <Input type="email" name="email" required placeholder="Your email" class="w-full" />
+                      </div>
+                      <div class="sm:col-span-2">
+                        <label for="phone-number" class="block text-sm/8 font-medium">Phone number</label>
+
+                        <Input type="tel" name="phone-number" placeholder="Your phone number" class="w-full" />
+                      </div>
+                      <div class="sm:col-span-2">
+                        <label for="message" class="block text-sm/8 font-medium">Message</label>
+
+                        <Textarea name="message" required placeholder="Your message" class="w-full" />
+                      </div>
                     </div>
-                    <div>
-                      <label for="last-name" class="block text-sm/8 font-medium">Last name</label>
-
-                      <Input type="text" name="last-name" required placeholder="Your last name" class="w-full" />
-                    </div>
-                    <div class="sm:col-span-2">
-                      <label for="email" class="block text-sm/8 font-medium">Email</label>
-
-                      <Input type="email" name="email" required placeholder="Your email" class="w-full" />
-                    </div>
-                    <div class="sm:col-span-2">
-                      <label for="phone-number" class="block text-sm/8 font-medium">Phone number</label>
-
-                      <Input type="tel" name="phone-number" placeholder="Your phone number" class="w-full" />
-                    </div>
-                    <div class="sm:col-span-2">
-                      <label for="message" class="block text-sm/8 font-medium">Message</label>
-
-                      <Textarea name="message" required placeholder="Your message" class="w-full" />
+                    <div class="mt-8 flex justify-end">
+                      <Button type="submit" variant="outline">Send message</Button>
                     </div>
                   </div>
-                  <div class="mt-8 flex justify-end">
-                    <Button type="submit" variant="outline">Send message</Button>
-                  </div>
-                </div>
-                <Turnstile siteKey="0x4AAAAAAA70S_YngNzk0TJI" />
-              </form>
-            {/if}
-          </div>
-        </section>
+                  <Turnstile siteKey="0x4AAAAAAA70S_YngNzk0TJI" />
+                </form>
+              {/if}
+            </div>
+          </section>
+
+          <!-- overlays -->
+          <div class="md:absolute z-20 top-10 inset-x-0 h-10 pointer-events-none bg-linear-to-b from-stone-50 to-transparent"></div>
+          <div class="md:absolute z-20 bottom-10 inset-x-0 h-10 pointer-events-none bg-linear-to-t from-stone-50 to-transparent"></div>
+        </div>
       </div>
     </div>
   </div>
